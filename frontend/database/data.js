da3b1b05890c9ff8e7bb27aa38d42af8a59ac4e9
yukioz <yukio.link@gmail.com>
2022-11-18 21:32:46 -0300

    iniciar frontend.
    Signed-off-by: yukioz <yukio.link@gmail.com>

---
+++ frontend/database/data.js
@@ -0,0 +1,46 @@
+const animals = [
+  {
+    nome: "rato",
+    power: 1,
+    image: "https://d3ugyf2ht6aenh.cloudfront.net/stores/140/298/products/kariba-cartas-011-a52e95a5eb858b95ea15948791715172-1024-1024.png"
+  },
+  {
+    nome: "rato",
+    power: 2,
+    image: "https://d3ugyf2ht6aenh.cloudfront.net/stores/140/298/products/kariba-cartas-011-a52e95a5eb858b95ea15948791715172-1024-1024.png"
+  },
+  {
+    nome: "rato",
+    power: 3,
+    image: "https://d3ugyf2ht6aenh.cloudfront.net/stores/140/298/products/kariba-cartas-011-a52e95a5eb858b95ea15948791715172-1024-1024.png"
+  },
+  {
+    nome: "rato",
+    power: 4,
+    image: "https://d3ugyf2ht6aenh.cloudfront.net/stores/140/298/products/kariba-cartas-011-a52e95a5eb858b95ea15948791715172-1024-1024.png"
+  },
+  {
+    nome: "rato",
+    power: 5,
+    image: "https://d3ugyf2ht6aenh.cloudfront.net/stores/140/298/products/kariba-cartas-011-a52e95a5eb858b95ea15948791715172-1024-1024.png"
+  },
+  {
+    nome: "rato",
+    power: 6,
+    image: "https://d3ugyf2ht6aenh.cloudfront.net/stores/140/298/products/kariba-cartas-011-a52e95a5eb858b95ea15948791715172-1024-1024.png"
+  },
+  {
+    nome: "rato",
+    power: 7,
+    image: "https://d3ugyf2ht6aenh.cloudfront.net/stores/140/298/products/kariba-cartas-011-a52e95a5eb858b95ea15948791715172-1024-1024.png"
+  },
+  {
+    nome: "rato",
+    power: 8,
+    image: "https://d3ugyf2ht6aenh.cloudfront.net/stores/140/298/products/kariba-cartas-011-a52e95a5eb858b95ea15948791715172-1024-1024.png"
+  },
+]
+
+const conections = [
+  
+]
